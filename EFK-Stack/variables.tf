e3cdfc5e5d5d5a28f3e91802a9d4e7cb07426375
Casey Davis <caseyjmdavis@gmail.com>
2021-06-21 16:55:32 -0400

    made tf vars sensitive

--- EFK-Stack/variables.tf
+++ EFK-Stack/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_access_key" {
 	type 		= string
 	description 	= "AWS access key id for tf-cloud IAM user"
+	sensitive 	= true
 }
 
 variable "aws_secret_key" {
 	type 		= string
 	description 	= "AWS secret access key for tf-cloud IAM user"
+	sensitive 	= true
 }

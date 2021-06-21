3a993d7e7eabec63cd1a42e3565742b73dd55964
Casey Davis <caseyjmdavis@gmail.com>
2021-06-21 16:43:45 -0400

    tf cloud backend

---
+++ EFK-Stack/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+	type 		= string
+	description 	= "AWS access key id for tf-cloud IAM user"
+}
+
+variable "aws_secret_key" {
+	type 		= string
+	description 	= "AWS secret access key for tf-cloud IAM user"
+}
